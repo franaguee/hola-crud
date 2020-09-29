72b451a03c9ae912ccdce39eff53d2fbb725c0e3
franaguee <67479979+franaguee@users.noreply.github.com>
2020-09-29 13:44:40 -0300

    Update note.js

--- api/routes/note.js
+++ api/routes/note.js
@@ -3,18 +3,18 @@ const router = express.Router();
 
 const Note = require('../models/Note');
 
-
+// POST /notes
 router.post('/notes', (req, res, next) => {
   const note = new Note({
-    title: req.body.title,
-    text: req.body.text
-  });
-  note.save((err, note) => {
-    if (err) return next(err);
-    res.status(201).json(note);
-  });
+      title: req.body.title,
+      text: req.body.text
+    });
+    note.save((err, note) => {
+      if (err) return next(err);
+      res.status(201).json(note);
+    });
 });
-
+// GET /notes
 router.get('/notes', (req, res, next) => {
   Note.find()                  // todos los docs de notes
     .select('_id title text')  // como SELECT en SQL
@@ -40,7 +40,7 @@ router.get('/notes', (req, res, next) => {
       });
     });
 });
-
+// GET /notes/id
 router.get('/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
     .select('_id title text createdAt updatedAt')  // todo menos __v
@@ -62,7 +62,7 @@ router.get('/notes/:id', (req, res, next) => {
       });
     });
 });
-
+// PUT /notes/id
 router.put('/notes/:id', (req, res, next) => {
   const note = {
     title: req.body.title,
@@ -79,7 +79,7 @@ router.put('/notes/:id', (req, res, next) => {
     res.status(200).json(note);
   });
 });
-
+// DELETE /notes/id
 router.delete('/notes/:id', (req, res, next) => {
   Note.findByIdAndRemove(req.params.id).exec((err, note) => {
     if (err) return next(err);
@@ -87,4 +87,5 @@ router.delete('/notes/:id', (req, res, next) => {
     res.status(200).json({ msg: 'Delete OK' });
   });
 });
+
 module.exports = router;
