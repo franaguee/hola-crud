ef005c24ac7f9ce1a1c1935220bd612229ef657f
franaguee <67479979+franaguee@users.noreply.github.com>
2020-09-29 13:37:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const cors     = require('cors');
 const morgan   = require('morgan');
 
+
 const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/notas';
 
 const app = express();
+
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
@@ -16,10 +18,13 @@ mongoose
     console.log(`DB connected @ ${db}`);
   })
   .catch(err => console.error(`Connection error ${err}`));
+
+// todo el middleware aca abajo y antes del listen
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use('/api', require('./api/routes/note'));
+
 app.use((req, res, next) => {
   const err = new Error('Not found');
   err.status = 404;
@@ -28,9 +33,10 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-
   res.json({ error: err.message });
 });
+
+// listen
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
-});  
+});
